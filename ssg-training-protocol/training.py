1569a0480d377c25762dc00c5c1e6c1ffaec992f
ssssg <81929297+ssg-qwq@users.noreply.github.com>
2023-10-04 16:15:14 +0800

    Update training.py

--- ssg-training-protocol/training.py
+++ ssg-training-protocol/training.py
@@ -7,7 +7,7 @@ import os
 
 class Trainner:
     def __init__(
-            self, map_db_dir="./dataset_db", server="http://0.0.0.0:3000/"
+        self, map_db_dir="./dataset_db", server="http://0.0.0.0:3000/"
     ) -> None:
         self.db = DataDB(map_db_dir)
         self.server = server
@@ -18,7 +18,7 @@ class Trainner:
         for e in range(epoch):
             try:
                 batch = self.db.get_batch()
-                running_loss = 0
+                running_loss = []
                 loop = tqdm.tqdm(enumerate(batch, 0))
                 for i, step_data in loop:
                     flow = step_data["data"]
@@ -35,23 +35,31 @@ class Trainner:
                     loss = requests.post(
                         self.server + "train/tx-data", json=package
                     ).json()["loss"]
-                    running_loss += loss
-
+                    running_loss.append(loss)
+                    if len(running_loss)>200:
+                        running_loss = running_loss[200:]
                     loop.set_description(
-                        f"epoch:[{e + 1}/{epoch}],step:[{i + 1}/{len(batch)}]"
+                        f"epoch:[{e + 1}/{epoch}],step:[{i+1}/{len(batch)}]"
                     )
-                    loop.set_postfix(loss=loss, loss_smooth=running_loss / (i + 1))
+                    loss_smooth = sum(running_loss) / (
+                        len(running_loss) if len(running_loss) < 200 else 200
+                    )
+                    loop.set_postfix(loss=loss, loss_smooth=loss_smooth)
 
-                    wandb.log({"loss": loss, "loss_smooth": running_loss / (i + 1)})
+                    wandb.log({"loss": loss, "loss_smooth": loss_smooth})
                 if e % n_epoch_save_ckpt == 0:
-                    requests.post(self.server + "/train/save-weight",
-                                  json={"model_name": f"ssg_training_protocol_epoch_{e}"})
+                    requests.post(
+                        self.server + "/train/save-weight",
+                        json={"model_name": f"ssg_training_protocol_epoch_{e}"},
+                    )
 
             except:
                 self.fault_time += 1
                 if self.fault_time == 20:
                     raise Exception("The maximum number of failures was reached.")
                 print(f"[Warning] Training Process Faulted, Total: {self.fault_time}")
+
+
 
 
 if __name__ == "__main__":
