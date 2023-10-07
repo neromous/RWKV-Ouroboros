88242b0ddad71d879ed8cdc705ef96a37de43c84
neromous <neromous@outlook.com>
2023-10-07 11:01:22 +0800

    upate

--- scripts/train_3b_sft.py
+++ scripts/train_3b_sft.py
@@ -3,12 +3,12 @@ import requests
 
 result = {}
 while result.get('loss', 2.0) > 0.5:
-    m = requests.post("http://0.0.0.0:3000/train/sft",
+    m = requests.post("http://0.0.0.0:40011/train/sft",
                       json={"message":"sample"})
     print(m.json())
     result = m.json()
 
 
 
-m = requests.post("http://0.0.0.0:3000/train/save-weight",json={})
+m = requests.post("http://0.0.0.0:40011/train/save-weight",json={})
 print(m.json())
